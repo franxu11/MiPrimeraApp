253a68faa9ac71e816fd461bb5b7d789115f6a89
Manel Simon <manelizzard@gmail.com>
2018-05-08 12:46:19 +0200

    More Login

--- app/src/main/java/com/android/teaching/miprimeraapp/login/view/LoginActivity.java
+++ app/src/main/java/com/android/teaching/miprimeraapp/login/view/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.android.teaching.miprimeraapp.login.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.teaching.miprimeraapp.ProfileActivity;
 import com.android.teaching.miprimeraapp.R;
 
 public class LoginActivity extends Activity {
@@ -23,19 +25,46 @@ public class LoginActivity extends Activity {
         passwordEditText = findViewById(R.id.password);
     }
 
+    /**
+     * Método que se ejecuta cuando el usuario pulsa en "Login"
+     *
+     * @param view  -
+     */
     public void onLogin(View view) {
-        // Get values
+        // Obtener valores
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
+        // Check empty values
         if (TextUtils.isEmpty(username)) {
-            // Show username empty error
-            usernameEditText.setError("Usuario no válido");
+            // El campo "username" está vacío
+            usernameEditText.setError(getString(R.string.username_error));
         } else if (TextUtils.isEmpty(password)) {
-            // Show password empty error
-            passwordEditText.setError("Contraseña no válida");
+            passwordEditText.setError(getString(R.string.password_error));
         } else {
             // Do login
+            Intent profileIntent = new Intent(this, ProfileActivity.class);
+            startActivity(profileIntent);
         }
+    }
+
+    /**
+     * Método que se ejecuta cuando el usuario pulsa en "Register"
+     *
+     * @param view  -
+     */
+    public void doRegister(View view) {
+        Intent registerIntent = new Intent(this, ProfileActivity.class);
+        startActivity(registerIntent);
+    }
+
+    /**
+     * Método que se ejecuta cuando el usuario pulsa en "Cancel"
+     *
+     * @param view  -
+     */
+    public void onCancel(View view) {
+        usernameEditText.setText("");
+        passwordEditText.setText("");
     }
 }
